17c0bdd414e9e99cc0d85ca1bc43dbac7ed03a8a
Humbat Yusubov <87598661+MrYusubov@users.noreply.github.com>
2024-10-18 23:22:54 +0400

    .

---
+++ TcpUnlimited/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+var ip = IPAddress.Parse("192.168.1.7");
+var port = 27001;
+
+var ep = new IPEndPoint(ip, port);
+
+var client = new TcpClient();
+try
+{
+    Console.WriteLine("Enter the path: ");
+    var path = Console.ReadLine();
+    client.Connect(ep);
+    if (client.Connected)
+    {
+        var fileName = Path.GetFileName(path!);
+        var fileNameBytes = System.Text.Encoding.UTF8.GetBytes(fileName);
+        var fileNameLength = BitConverter.GetBytes(fileNameBytes.Length);
+
+        var networkStream = client.GetStream();
+
+        networkStream.Write(fileNameLength, 0, fileNameLength.Length);
+        networkStream.Write(fileNameBytes, 0, fileNameBytes.Length);
+
+        using (var fs = new FileStream(path!, FileMode.Open, FileAccess.Read))
+        {
+            var buffer = new byte[65536];
+            int len;
+            while ((len = fs.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                networkStream.Write(buffer, 0, len);
+            }
+        }
+
+        networkStream.Close();
+        client.Close();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ TcpUnlimited/TcpUnlimited/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var ip = IPAddress.Parse("192.168.1.7");
+var port = 27001;
+var ep = new IPEndPoint(ip, port);
+var listener = new TcpListener(ep);
+try
+{
+    listener.Start();
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+        _ = Task.Run(() =>
+        {
+            var networkStream = client.GetStream();
+            var remoteEp = client.Client.RemoteEndPoint as IPEndPoint;
+            var directoryPath = Path.Combine(Environment.CurrentDirectory, remoteEp!.Address.ToString()!);
+            if (!Directory.Exists(directoryPath)) { Directory.CreateDirectory(directoryPath); }
+            var fileNameLengthBuffer = new byte[4];
+            networkStream.Read(fileNameLengthBuffer, 0, 4);
+            int fileNameLength = BitConverter.ToInt32(fileNameLengthBuffer, 0);
+            var fileNameBuffer = new byte[fileNameLength];
+            networkStream.Read(fileNameBuffer, 0, fileNameLength);
+            string fileName = System.Text.Encoding.UTF8.GetString(fileNameBuffer);
+            var path = Path.Combine(directoryPath, fileName);
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                int len = 0;
+                var buffer = new byte[65536];
+                while ((len = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fs.Write(buffer, 0, len);
+                }
+            }
+            Console.WriteLine("File Received!");
+            client.Close();
+        });
+    }
+
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
