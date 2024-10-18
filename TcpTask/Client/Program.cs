974bb28d71774c854bd8b5fe67a487e583157975
Humbat Yusubov <87598661+MrYusubov@users.noreply.github.com>
2024-10-18 22:04:58 +0400

    .

---
+++ TcpTask/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+
+var ip = IPAddress.Parse("192.168.1.7");
+var port = 27001;
+
+var ep = new IPEndPoint(ip, port);
+
+try
+{
+    while (true)
+    {
+        _ = Task.Run(() =>
+        {
+            using (var client = new TcpClient())
+            {
+                client.Connect(ep);
+                var networkStream = client.GetStream();
+                using (var memoryStream = new MemoryStream())
+                {
+                    Bitmap memoryImage = new Bitmap(1920, 1080);
+                    Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+                    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+                    memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+                    memoryImage.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                    var buffer = memoryStream.ToArray();
+                    networkStream.Write(buffer, 0, buffer.Length);
+                    networkStream.Flush();
+                }
+                Console.WriteLine("Sent Successfully!");
+                client.Close();
+            }
+        });
+        await Task.Delay(10000);
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ TcpTask/TcpTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var ip = IPAddress.Parse("192.168.1.7");
+var port = 27001;
+var ep = new IPEndPoint(ip, port);
+var listener = new TcpListener(ep);
+try
+{
+    listener.Start();
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+        _ = Task.Run(() =>
+        {
+            var networkStream = client.GetStream();
+            var remoteEp = client.Client.RemoteEndPoint as IPEndPoint;
+            var directoryPath = Path.Combine(Environment.CurrentDirectory, remoteEp!.Address.ToString()!);
+            if (!Directory.Exists(directoryPath)) { Directory.CreateDirectory(directoryPath); }
+            var path = Path.Combine(directoryPath, $"{DateTime.Now.ToString("yyyy/MM/dd/HH/mm/ss")}.png");
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                int len = 0;
+                var buffer = new byte[1024];
+                while ((len = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fs.Write(buffer, 0, len);
+                }
+                fs.Close();
+            }
+            Console.WriteLine("File Received!");
+            client.Close();
+        });
+    }
+   
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
