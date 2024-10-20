40e2f4bc79acd675173042538c696109329fa8c5
Humbat Yusubov <87598661+MrYusubov@users.noreply.github.com>
2024-10-20 22:31:09 +0400

    .

---
+++ UDPTask/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var endPoint = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 27001);
+
+var server = new UdpClient(endPoint);
+
+Console.WriteLine("Server is running");
+var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    var directoryPath = Path.Combine(Environment.CurrentDirectory, endPoint!.Address.ToString()!);
+    if (!Directory.Exists(directoryPath)) { Directory.CreateDirectory(directoryPath); }
+    var fileNameBytes = server.Receive(ref remoteEndPoint);
+    var fileLengthBytes = server.Receive(ref remoteEndPoint);
+    var fileName = Encoding.UTF8.GetString(fileNameBytes);
+    int fileLength = int.Parse(Encoding.UTF8.GetString(fileLengthBytes));
+
+    var path = $"{DateTime.Now:HH.mm.ss}{Path.GetExtension(fileName)}";
+    var fullPath=Path.Combine(directoryPath, path);
+
+    using (var stream = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.Write))
+    {
+        var totalRecievedBytes = 0;
+        while (true)
+        {
+            var fileBytes = server.Receive(ref remoteEndPoint);
+            stream.Write(fileBytes);
+            totalRecievedBytes += fileBytes.Length;
+            server.Send(null, remoteEndPoint);
+            if (fileLength == totalRecievedBytes)
+                break;
+        }
+    }
+
+    Console.WriteLine("File Downloaded");
+}
---
+++ UDPTask/UDPTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var endPoint = new IPEndPoint(IPAddress.Parse("192.168.1.7"), 27001);
+var rep = new IPEndPoint(IPAddress.Any, 0);
+var client = new UdpClient();
+
+Console.WriteLine("Enter the Path: ");
+var path = Console.ReadLine();
+
+FileInfo file = new FileInfo(path);
+var fileNameBytes = Encoding.UTF8.GetBytes(file.Name);
+client.Send(fileNameBytes, fileNameBytes.Length, endPoint);
+var fileLengthBytes = Encoding.UTF8.GetBytes(file.Length.ToString());
+client.Send(fileLengthBytes, fileLengthBytes.Length, endPoint);
+
+using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+{
+    var bytes = new byte[65000];
+    var len = 0;
+    while ((len = fs.Read(bytes, 0, bytes.Length)) > 0)
+    {
+        client.Send(bytes, len, endPoint);
+        client.Receive(ref rep);
+    }
+}
+
+Console.WriteLine("File sent");
+Console.ReadKey();
